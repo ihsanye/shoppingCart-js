6dbc499d59f7cae8006bf0973c5a9285705dc24f
ihsanye <ihsany98@gmail.com>
2024-04-21 19:48:06 +0300

    next step more functionality

--- script.js
+++ script.js
@@ -112,5 +112,24 @@ class ShoppingCart {
         const product = products.find((item) => item.id === id);
         const { name, price } = product;
         this.items.push(product);
+
+        const totalCountPerProduct = {};
+        this.items.forEach((dessert) => {
+            totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+        })
+
+        const currentProductCount = totalCountPerProduct[product.id];
+        const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+
+        currentProductCount > 1
+            ? currentProductCountSpan.textContent = `${currentProductCount}x`
+            : productsContainer.innerHTML += `
+      <div id="dessert${id}" class="product">
+        <p>
+          <span class="product-count" id="product-count-for-id${id}"></span>${name}
+        </p>
+        <p>${price}</p>
+      </div>
+      `;
     }
 };
